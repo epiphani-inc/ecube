943053b55d87505eed0721fd7d6d56ba3efd1cfb
Praveen Madhav <praveen@epiphani.io>
2020-08-12 08:13:12 +0000

    Fixed missing dependencies. Fixed sub-package files not being included into the package.

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ setup(
     license='MIT',
     scripts=['e3'],
     install_requires=[
+        'tabulate>=0.8.7',
+        'PyYAML>=5.3.1',
+        'Jinja2>=2.11.2',
         'websocket-client>=0.57.0',
         'warrant>=0.6.1',
         'configparser>=5.0.0',
@@ -24,7 +27,7 @@ setup(
     url='https://github.com/epiphani-inc/ecube',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    packages=find_packages(include=["ecube.*"]),
+    packages=find_packages(include=["ecube", "ecube.*"]),
     include_package_data=True,
     description="epiphani CLI for registering connectors & executing scripts in a sandbox",
 )
